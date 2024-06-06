71e799d04f2a7b1868bf480cb8eeae756c9a07f2
Hojin Yang <deokji53@gmail.com>
2024-06-06 00:27:51 +0000

    rename c to input

--- hd/hd.c
+++ hd/hd.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-int transition(int s, int c);
+int transition(int s, int input);
 
 int main(void)
 {
@@ -21,10 +21,10 @@ int main(void)
   return 0;
 }
 
-int transition(int s, int c) {
+int transition(int s, int input) {
   switch (s) {
     case 0: {
-      switch (c) {
+      switch (input) {
         case '#': return 1;
         case '\r': return 4;
         case '\n': return 0;
@@ -32,20 +32,20 @@ int transition(int s, int c) {
       }
     }
     case 1: {
-      switch (c) {
+      switch (input) {
         case ' ': return 2;
         default: return 9;
       }
     }
     case 2: {
-      switch (c) {
+      switch (input) {
         case '\r': return 3;
         case '\n': return 0;
         default: return 2;
       }
     }
     case 3: {
-      switch (c) {
+      switch (input) {
         case '\n': return 0;
         default: return 2;
       }
@@ -54,33 +54,33 @@ int transition(int s, int c) {
       return 0;
     }
     case 5: {
-      switch (c) {
+      switch (input) {
         case '\r': return 6;
         case '\n': return 0;
         default: return 7;
       }
     }
     case 6: {
-      switch (c) {
+      switch (input) {
         case '\n': return 0;
         default: return 5;
       }
     }
     case 7: {
-      switch (c) {
+      switch (input) {
         case '\r': return 8;
         case '\n': return 0;
         default: return 7;
       }
     }
     case 8: {
-      switch (c) {
+      switch (input) {
         case '\n': return 0;
         default: return 7;
       }
     }
     case 9: {
-      switch (c) {
+      switch (input) {
         case '\r': return 8;
         case '\n': return 0;
         default: return 7;
