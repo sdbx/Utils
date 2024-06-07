902819c3ccc20cfa252ae37a041ee41e49a0dd30
andjsrk <andjsrk0213@gmail.com>
2024-06-08 01:54:43 +0900

    correct the behavior of hhss.

--- hhss/hutsori.hs
+++ hhss/hutsori.hs
@@ -1,5 +1,6 @@
 import Data.List
 import System.IO
+import System.Environment
 
 userFile = "usr.dat"
 sentenceFile = "hsr.dat"
@@ -20,8 +21,10 @@ isValidLine l =
   not $ "#" `isPrefixOf` l || null l
 
 main = do
+  (rawSentenceCount:_) <- getArgs
+  let sentenceCount = read rawSentenceCount :: Int
   sentence <- readFileAsUtf8 sentenceFile
   usersRaw <- readFileAsUtf8 userFile
   let user = head $ lines usersRaw
-  let validLines = filter isValidLine . lines $ sentence
+  let validLines = take sentenceCount . filter isValidLine . lines $ sentence
   mapM_ (putStrLn . replace user) validLines
