3ea8d80f84db820d6e86f38e6be5787998817851
Seunghoon Lee <lshqqytiger@naver.com>
2024-06-13 16:24:29 +0900

    c-cpp (2) : revert formatting changes

--- hd/cpp/hd.cpp
+++ hd/cpp/hd.cpp
@@ -6,24 +6,19 @@ using namespace std;
 
 void trim(string &s);
 
-int main()
-{
+int main() {
   string line;
-  while (getline(cin, line))
-  {
+  while (getline(cin, line)) {
     trim(line);
-    if (line.length() != 0 && !line.starts_with("# "))
-    {
+    if (line.length() != 0 && !line.starts_with("# ")) {
       cout << "# ";
     }
     cout << line << endl;
   }
 }
 
-inline void trim(string &s)
-{
-  auto not_space = [](unsigned char ch)
-  { return !isspace(ch); };
+inline void trim(string &s) {
+  auto not_space = [](unsigned char ch) { return !isspace(ch); };
 
   s.erase(s.begin(), find_if(s.begin(), s.end(), not_space));
   s.erase(find_if(s.rbegin(), s.rend(), not_space).base(), s.end());
