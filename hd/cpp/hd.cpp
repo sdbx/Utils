b05b32b67234f8cdc2d8cb797f29c6fda7635d03
Seunghoon Lee <lshqqytiger@naver.com>
2024-06-13 16:20:39 +0900

    c-cpp (1) : rewrite gitignore, makefile

--- hd/cpp/hd.cpp
+++ hd/cpp/hd.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 void trim(string &s);
 
-int main() {
+int main()
+{
   string line;
-  while (getline(cin, line)) {
+  while (getline(cin, line))
+  {
     trim(line);
-    if (line.length() != 0 && !line.starts_with("# ")) {
+    if (line.length() != 0 && !line.starts_with("# "))
+    {
       cout << "# ";
     }
     cout << line << endl;
   }
 }
 
-inline void trim(string &s) {
-  auto not_space = [](unsigned char ch) { return !isspace(ch); };
+inline void trim(string &s)
+{
+  auto not_space = [](unsigned char ch)
+  { return !isspace(ch); };
 
   s.erase(s.begin(), find_if(s.begin(), s.end(), not_space));
   s.erase(find_if(s.rbegin(), s.rend(), not_space).base(), s.end());
