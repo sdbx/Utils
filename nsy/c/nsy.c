46cb6eca248344f4a9155ec7d4be6994e192dd5a
Doohyeon Won (Daniel Harold Won) (Cor) <logicseeker@naver.com>
2024-06-15 18:56:34 +0900

    provide more delimiters: period, ellipsis, plus, comma, heart

--- nsy/c/nsy.c
+++ nsy/c/nsy.c
@@ -28,10 +28,16 @@
 
 void raise_err(char *err_msg);
 int inspect_char(int ch);
+/* delimiters */
 void print_excl_mark(void);
 void print_ques_mark(void);
 void print_excl_mark_u(void);
 void print_ques_mark_u(void);
+void print_period(void);
+void print_ellipsis(void);
+void print_plus(void);
+void print_comma_twice(void);
+void print_heart(void);
 
 int main(int argc, char *argv[]) {
    if (argc != 2)
@@ -39,14 +45,24 @@ int main(int argc, char *argv[]) {
 
    void (*print_mark)(void);
 
-   if (strcmp(argv[1], "e") == 0)
-      print_mark = print_excl_mark;
-   else if (strcmp(argv[1], "q") == 0)
-      print_mark = print_ques_mark;
-   else if (strcmp(argv[1], "eu") == 0)
+   if (strncmp(argv[1], "eu", 2) == 0)
       print_mark = print_excl_mark_u;
-   else if (strcmp(argv[1], "qu") == 0)
+   else if (strncmp(argv[1], "qu", 2) == 0)
       print_mark = print_ques_mark_u;
+   else if (strncmp(argv[1], "pl", 2) == 0)
+      print_mark = print_plus;
+   else if (strncmp(argv[1], "el", 2) == 0)
+      print_mark = print_ellipsis;
+   else if (strncmp(argv[1], "e", 1) == 0)
+      print_mark = print_excl_mark;
+   else if (strncmp(argv[1], "q", 1) == 0)
+      print_mark = print_ques_mark;
+   else if (strncmp(argv[1], "p", 1) == 0)
+      print_mark = print_period;
+   else if (strncmp(argv[1], "c", 1) == 0)
+      print_mark = print_comma_twice;
+   else if (strncmp(argv[1], "h", 1) == 0)
+      print_mark = print_heart;
    else
       raise_err("nsy: an unknown argument.");
    
@@ -123,4 +139,29 @@ inline void print_excl_mark_u(void) {
 /* ?' */
 inline void print_ques_mark_u(void) {
    fputs(" \xC2\xBF ", stdout);    /* 0x00BF */
+}
+
+/* . */
+void print_period(void) {
+   putchar('.');  /* 0x002E */
+}
+
+/* ... */
+void print_ellipsis(void) {
+   fputs("\xE2\x80\xA6", stdout);   /* 0x2026 */
+}
+
+/* + */
+void print_plus(void) {
+   putchar('+');  /* 0x002B */
+}
+
+/* ,, */
+void print_comma_twice(void) {
+   fputs(",,", stdout);    /* 0x002C */
+}
+
+/* ♥ */
+void print_heart(void) {
+   fputs("\xE2\x99\xA5", stdout);    /* 0x2665 */
 }
