4f0eb77f5c1e172be62cc0697dd08a4546f263a6
Doohyeon Won <logicseeker@naver.com>
2024-06-17 19:53:40 +0900

    [Fix] Modify python implementations of btn and nsy (#33)

--- nsy/python/nsy.py
+++ nsy/python/nsy.py
@@ -1,27 +1,34 @@
 import sys
+import string
 
 def main(mark_kind):
-    text = input().replace(" ", "")
+    text = sys.stdin.read()
     if len(text) == 0:
-        print("nsy: EOF detected.")
-        sys.exit(1)
+        raise_err('nsy: EOF detected.')
+    
+    text = text.translate(str.maketrans('', '', string.whitespace))
     match mark_kind:
-        case "e":
-            print('',*text,'',sep='！')
-        case "q":
-            print('',*text,'',sep='？')
-        case "eu":
-            print('',*text,'',sep=' ¡ ')
-        case "qu":
-            print('',*text,'',sep=' ¿ ')
+        case 'e':
+            print('', *text, '', sep='！')
+        case 'q':
+            print('', *text, '', sep='？')
+        case 'eu':
+            print('', *text, '', sep=' ¡ ')
+        case 'qu':
+            print('', *text, '', sep=' ¿ ')
         case _:
-            print("nsy: unknown delimiter.")
+            raise_err('nsy: an unknown delimiter.')
 
-if len(sys.argv) == 1:
-    print("nsy: argc != 2.")
-    print("Usage : python nsy.py MARK_KIND")
-    print("  where\n\tMARK_KIND = e | q | eu | qu")
+def raise_err(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
     sys.exit(1)
+
+if len(sys.argv) != 2:
+    raise_err(
+        'nsy: argc != 2.',
+        'Usage : python nsy.py MARK_KIND',
+        '  where\n\tMARK_KIND = e | q | eu | qu',
+        sep='\n')
     
 mark_kind = sys.argv[1]
 main(mark_kind)
