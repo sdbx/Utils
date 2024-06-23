3b69d5132a48dbd8dc1f7fd74cfb0829f125aaa2
Doohyeon Won (Daniel Harold Won) (Cor) <logicseeker@naver.com>
2024-06-24 01:16:52 +0900

    support-multi-output (10) : introduce javadoc

--- test/test.h
+++ test/test.h
@@ -11,41 +11,94 @@
 #include "test.macro"
 #include "test.typedef"
 
-// raise_err() prints an error message to stderr.
+/**
+ * @brief prints an error message to stderr.
+ * 
+ * @param err_msg an error message to be printed
+ * @param ... variable argument list
+ */
 void raise_err(const char *err_msg, ...);
 
-// check_correct_execution() checks whether the program has been executed
-// with the correct number of command line arguments.
+/**
+ * @brief checks whether the program has been executed with the correct number of command line arguments.
+ * 
+ * @param argc the number of command line arguments
+ */
 void check_correct_execution(int argc);
 
-// acquire_testcases() gets testcases, write the length of testcases via
-// testcases_len_ptr, and returns the pointer to the first testcase.
-// The return value of this function must be `free`d once it's no longer needed.
+/**
+ * @brief gets testcases, write the length of testcases via *testcases_len_ptr, and returns the pointer to the first testcase.
+ * 
+ * @param testcase_len_ptr a pointer to testcases_len
+ * @return a pointer to an array of testcases
+ * 
+ * @details The return value of this function must be `free`d once it's no longer needed.
+ */
 testcase *acquire_testcases(int *testcases_len_ptr);
 
-// perform_test() conducts tests with testcases.
+/**
+ * @brief conducts tests with testcases.
+ * 
+ * @param testcases a pointer to an array of testcases
+ * @param testcases_len the length of the testcases array
+ * @param executable_name the name of the program to be tested
+ * @return true if every testcases were successful; otherwise false.
+ */
 bool perform_test(testcase *testcases, int testcases_len, const char *executable_name);
 
-// make_command() `malloc`s a string, write a command to it, and returns it.
-// The return value of this function must be `free`d once it's no longer needed.
+/**
+ * @brief `malloc`s a string, write a command to it, and returns it.
+ * 
+ * @param tc_ptr a pointer to a testcase struct that has necessary information to make the command to execute
+ * @param executable_name the name of the program to be tested
+ * @return a command to be executed soon; refer to execute_command().
+ * 
+ * @details The return value of this function must be `free`d once it's no longer needed.
+ */
 char *make_command(const testcase *tc_ptr, const char *executable_name);
 
-// execute_command() runs the command acquired from the call of make_command().
+/**
+ * @brief runs the command acquired from the call of make_command().
+ * 
+ * @param cmd a command to execute
+ * @param tempfile a temporary file to hold the result of the execution of the command
+ */
 void execute_command(const char *cmd, FILE *tempfile);
 
-// acquire_output() reads lines in the tempfile, writes them in a string, and returns it.
-// The return value of this function must be `free`d once it's no longer needed.
+/**
+ * @brief reads lines in the tempfile, writes them in a string, and returns it.
+ * 
+ * @param tempfile a temporary file which contains the result of the execution of a command
+ * @return a string which is the output from an executable that was run with a command
+ * 
+ * @details The return value of this function must be `free`d once it's no longer needed.
+ */
 char *acquire_output(FILE *tempfile);
 
-// evaluate_testcase() checks if there is at least one expected output that
-// matches the actual output.
+/**
+ * @brief checks if there is at least one expected output that matches the actual output.
+ * 
+ * @param params_ptr a pointer to a struct which contains things necessary to call this function
+ * @return true if a testcase was successful; otherwise false.
+ */
 bool evaluate_testcase(const eval_tc_params *params_ptr);
 
-// prints the result of the testcase.
+/**
+ * @brief prints the result of the testcase.
+ * 
+ * @param params_ptr a pointer to a struct which contains things necessary to call this function
+ */
 void display_testcase_result(const disp_tc_res_params *params_ptr);
 
-// convert_linefeed() replaces \n character(s) in src with replace_str.
-// The return value of this function must be `free`d once it's no longer needed.
+/**
+ * @brief replaces a line-feed character(s) in src with replace_str.
+ * 
+ * @param src a source string
+ * @param replace_str a string used to replace a `\\n` character.
+ * @param len this function reads characters from `src` until (1) it reads the null-terminating character or (2) it reads `len` characters.
+ * 
+ * @details The return value of this function must be `free`d once it's no longer needed.
+ */
 char *convert_linefeed(const char *src, const char *replace_str, int len);
 
 #endif
--- test/test_maker.h
+++ test/test_maker.h
@@ -15,37 +15,76 @@
 #define TESTCASE_FILE_POSTFIX    ".modified"
 #define NUM_DQ_MARKS_BEFORE_OUTPUTS_ARR    6
 
-// raise_err() prints an error message to stderr.
+/**
+ * @brief prints an error message to stderr.
+ * 
+ * @param err_msg an error message to be printed
+ * @param ... variable argument list
+ */
 void raise_err(const char *err_msg, ...);
 
-// check_correct_execution() checks whether the program has been executed
-// with the correct number of command line arguments.
+/**
+ * @brief checks whether the program has been executed with the correct number of command line arguments.
+ * 
+ * @param argc the number of command line arguments
+ */
 void check_correct_execution(int argc);
 
-// make_dest_filename() returns a string which is the name of the modified testcase file.
-// The return value of this function must be `free`d once it's no longer needed.
+/**
+ * @brief returns a string which is the name of the modified testcase file.
+ * 
+ * @param testcase_filename the name of an original testcase file
+ * @return the name which a modified testcase file will have
+ *
+ * @details The return value of this function must be `free`d once it's no longer needed.
+ */
 char *make_dest_filename(const char *testcase_filename);
 
-// preprocess_testcase() reads a testcase file and makes a modified version of it.
+/**
+ * @brief reads a testcase file and makes a modified version of it.
+ * 
+ * @param src_name the name of an original testcase file
+ * @param dest_name the name of the modified version of the original testcase file
+ */
 void preprocess_testcase(const char *src_name, const char *dest_name);
 
-// count_outputs_len() counts the length of the 'outputs' array that each testcase has
-// and writes it to lengths[i].
+/**
+ * @brief counts the length of the 'outputs' array that each testcase has and writes it to lengths[i].
+ * 
+ * @param lengths a pointer to an array which will contain how many expected outputs each testcase has
+ * @param src an original testcase file
+ */
 void count_outputs_len(int lengths[], FILE *src);
 
-// write_modified_testcase() writes a modified testcase file.
+/**
+ * @brief writes a modified testcase file.
+ * 
+ * @param lengths a pointer to an array containing how many expected outputs each testcase has
+ * @param src an original testcase file
+ * @param dest a modified testcase file
+ */
 void write_modified_testcase(int lengths[], FILE *src, FILE *dest);
 
-// write_modified_file() makes a modified 'test.c' file.
+/**
+ * @brief makes a modified 'test.c' file.
+ * 
+ * @param testcase_modified_filename the name of a modified testcase file
+ */
 void write_modified_file(const char *testcase_modified_filename);
 
-// build_executable() builds the executable from the 'test.modified.c'.
+/**
+ * @brief builds the executable from the 'test.modified.c'.
+ */
 void build_executable(void);
 
-// cleanup() erases temporary files.
+/**
+ * @brief erases temporary files.
+ */
 void cleanup(char *dest_filename);
 
-// announce() prints a message notifying that the program has finished its work.
+/**
+ * @brief prints a message notifying that the program has finished its work.
+ */
 void announce(void);
 
 #endif
