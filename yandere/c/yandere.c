271c2d5a44453f3d58f41cebc20c7436e04e860b
Doohyeon Won <logicseeker@naver.com>
2026-05-21 02:19:43 +0900

    feat: prevent escapes via signals

--- yandere/c/yandere.c
+++ yandere/c/yandere.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <signal.h>
+
+typedef void sighandler_t(int sig);
+
+static inline void trigger_loop(const char *str);
+static inline sighandler_t handle_sigint;
+static inline sighandler_t handle_sigquit;
+static inline sighandler_t handle_sigterm;
+
+const char *msgs[] = {
+   "이제 내가 싫어진거야? ",
+   "도망가려고? ",
+   "킥킥, 고작 그런 수로 벗어날 수 있을 거라 생각했어? ",
+   "어디가? 설마 도망가는건 아니지? "
+};
 
 int main(void) {
-   for (;;)
-      printf("이제 내가 싫어진거야? ");
-      
+   signal(SIGINT , handle_sigint);
+   signal(SIGQUIT, handle_sigquit);
+   signal(SIGTERM, handle_sigterm);
+   trigger_loop(msgs[0]);
    return 0;
+}
+
+static inline void trigger_loop(const char *str) {
+   for (;;) fputs(str, stdout);
+}
+
+static inline void handle_sigint(int sig) {
+   (void) sig;
+   trigger_loop(msgs[1]);
+}
+
+static inline void handle_sigquit(int sig) {
+   (void) sig;
+   trigger_loop(msgs[2]);
+}
+
+static inline void handle_sigterm(int sig) {
+   (void) sig;
+   trigger_loop(msgs[3]);
 }
