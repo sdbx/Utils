c44bdbb161b930b8be3c9e7c435013d46f9102c5
Doohyeon Won <logicseeker@naver.com>
2026-05-29 07:39:05 +0900

    fix: not reinstall signal handlers

--- yandere/c/sighandlers.c
+++ yandere/c/sighandlers.c
@@ -3,17 +3,14 @@
 extern void sighandle_sigint(int sig) {
    (void) sig;
    event = Event_sigint;
-   safe_signal(sig, sighandle_sigint);
 }
 
 extern void sighandle_sigquit(int sig) {
    (void) sig;
    event = Event_sigquit;
-   safe_signal(sig, sighandle_sigquit);
 }
 
 extern void sighandle_sigterm(int sig) {
    (void) sig;
    event = Event_sigterm;
-   safe_signal(sig, sighandle_sigterm);
 }
